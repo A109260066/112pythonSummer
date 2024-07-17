b54548fb0cde8c7bd411d6688c810b73ebb522d6
A109260066 <a109260066@mail.shu.edu.tw>
2024-07-17 14:44:29 +0800

    113-07-15

--- 02-Karel/karel_02.py
+++ 02-Karel/karel_02.py
@@ -1,8 +1,28 @@
 from stanfordkarel import *
 import os
 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def turn_around():
+    turn_left()
+    turn_left()    
+
+def fill_pothole():
+    turn_right()
+    move()
+    put_beeper()
+    turn_around()
+    move()
+    turn_right()
+
 def main():
     """ Karel code goes here! """
+    move()
+    fill_pothole()
+    move()
     #pass
 
 
